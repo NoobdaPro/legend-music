001f9f59cdd0f6590cd70b6ce39c8686a41647c4
NoobdaPro <98887725+NoobdaPro@users.noreply.github.com>
2022-03-05 20:07:06 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -11,24 +11,16 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 async def start_(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://telegra.ph/file/c70635cc28eec97a43f40.jpg",
-        caption=f"""**𝐓𝐡𝐢𝐬 𝐈𝐬 𝐀𝐝𝐯𝐚𝐧𝐜𝐞 🥀𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐌𝐮𝐬𝐢𝐜 𝐁𝐨𝐭 𝐑𝐮𝐧 𝐎𝐧 𝐏𝐫𝐢𝐯𝐚𝐭𝐞 🥀 𝐕𝐩𝐬 💫𝐒𝐞𝐫𝐯𝐞𝐫 🌎 𝐅𝐞𝐞𝐥 ❤️ 𝐇𝐢𝐠𝐡 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 𝐌𝐮𝐬𝐢𝐜 🎧 𝐈𝐧 𝐕𝐜**""",
+        caption=f"""**𝐓𝐡𝐢𝐬 𝐈𝐬 𝐀𝐝𝐯𝐚𝐧𝐜𝐞 🥀𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐌𝐮𝐬𝐢𝐜 𝐁𝐨𝐭\n\n⚜𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝙱𝚢 - 𝙷𝚊𝚛𝚜𝚑𝚞_𝚡𝙳**""",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "♥️ Creator ♥️", url=f"https://t.me/DhrubaXD")
+                        "♥️ Creator ♥️", url=f"https://t.me/Harshu_xD")
                ],
                 [
                     InlineKeyboardButton(
-                        "Repo ✨", url=f"https://github.com/LEGENDARY-OS/MUSIC-BOT")
-               ], 
-                [
-                    InlineKeyboardButton(
-                        "👨‍💻 Support", url=f"https://t.me/Official_K_LegendBot")
-               ],
-                [
-                    InlineKeyboardButton(
-                        "💝 Help 💝", url=f"https://t.me/Legend_K_Userbot")
+                        "Channel🔥", url=f"https://t.me/StarterChannel")
                 ]
                 
            ]
@@ -38,13 +30,13 @@ async def start_(client: Client, message: Message):
 @Client.on_message(command(["repo"]) & filters.group & ~filters.edited)
 async def help(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/11a149401351a322dc1bc.jpg",
+        photo=f"https://telegra.ph/file/389f418594b430aad58a6.jpg",
         caption=f"""""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "💥 Repo 💞", url=f"https://github.com/LEGENDARY-OS/MUSIC-BOT")
+                        "💥 Managed By 💞", url=f"https://t.me/Harshu_xD")
                 ]
             ]
         ),
