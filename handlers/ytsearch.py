4ab1118a650a2029b47434df23b8854f81585ca2
NoobdaPro <98887725+NoobdaPro@users.noreply.github.com>
2022-03-05 19:41:39 +0530

    Update ytsearch.py

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -33,6 +33,7 @@ async def ytsearch(_, message: Message):
             text += f"Views - {results[i]['views']}\n"
             text += f"Channel - {results[i]['channel']}\n"
             text += f"https://youtube.com{results[i]['url_suffix']}\n\n"
+            text += f"⚜𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝙱𝚢 - 𝙷𝚊𝚛𝚜𝚑𝚞_𝚡𝙳\n"
             i += 1
         await m.edit(text, disable_web_page_preview=True)
     except Exception as e:
