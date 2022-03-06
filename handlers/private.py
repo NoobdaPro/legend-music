8e083cb732de7cb0c6cedb0011ce88f146adb1d8
LEGEND-AI <Krishna045Jaiswal@gmail.com>
2022-03-06 04:10:11 +0000

    pylint: auto fixes

--- handlers/private.py
+++ handlers/private.py
@@ -1,10 +1,7 @@
-import asyncio
-from time import time
-from datetime import datetime
-from helpers.filters import command
 from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
+from helpers.filters import command
 
 
 @Client.on_message(command("start") & filters.private & ~filters.edited)
@@ -12,20 +9,14 @@ async def start_(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://telegra.ph/file/c70635cc28eec97a43f40.jpg",
         caption=f"""**𝐓𝐡𝐢𝐬 𝐈𝐬 𝐀𝐝𝐯𝐚𝐧𝐜𝐞 🥀𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐌𝐮𝐬𝐢𝐜 𝐁𝐨𝐭\n\n⚜𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝙱𝚢 - 𝙷𝚊𝚛𝚜𝚑𝚞_𝚡𝙳**""",
-    reply_markup=InlineKeyboardMarkup(
+        reply_markup=InlineKeyboardMarkup(
             [
-                [
-                    InlineKeyboardButton(
-                        "♥️ Creator ♥️", url=f"https://t.me/Harshu_xD")
-               ],
-                [
-                    InlineKeyboardButton(
-                        "Channel🔥", url=f"https://t.me/StarterChannel")
-                ]
-                
-           ]
-       ),
+                [InlineKeyboardButton("♥️ Creator ♥️", url=f"https://t.me/Harshu_xD")],
+                [InlineKeyboardButton("Channel🔥", url=f"https://t.me/StarterChannel")],
+            ]
+        ),
     )
+
 
 @Client.on_message(command(["repo"]) & filters.group & ~filters.edited)
 async def help(client: Client, message: Message):
@@ -33,11 +24,6 @@ async def help(client: Client, message: Message):
         photo=f"https://telegra.ph/file/389f418594b430aad58a6.jpg",
         caption=f"""""",
         reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "💥 Managed By 💞", url=f"https://t.me/Harshu_xD")
-                ]
-            ]
+            [[InlineKeyboardButton("💥 Managed By 💞", url=f"https://t.me/Harshu_xD")]]
         ),
     )
